f260fe782843fabf32bd8c27d3c2f062289b9e3f
yahyademirbas <yahyademirbas@yahoo.com>
2021-04-28 01:15:57 +0300

    Spirtes added, notes taken.

---
+++ Assets/Scripts/Notes.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    /*
+     
+    -- 14 Move. Bach E Major Partita Preludio will be played with chiptune
+
+    -- Black Dot will be pink in 14 gradual steps.
+
+    -- Big Circle Trigger Collider
+
+    -- Collision collider on small circles
+
+    -- Texts on circles.
+
+    -- Rotation. 
+
+    -- Shake Screen when player fails. Also change de background color
+
+    --
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     
+     */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
